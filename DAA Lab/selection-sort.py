890dffaef75098714bc97320da055c394e19a0d1
shreymakadiya18 <86375553+shreymakadiya18@users.noreply.github.com>
2022-05-04 01:22:23 +0530

    Update selection-sort.py

--- DAA Lab/selection-sort.py
+++ DAA Lab/selection-sort.py
@@ -1,12 +1,19 @@
-A = [64,25,12,22,11]
+# Python program for implementation of Selection Sort
+A = [64, 25, 12, 22, 11]
 
+# Traverse through all array elements
 for i in range(len(A)):
-    min_idx = i
-    for j in range(i+1,len(A)):
-        if A[min_idx]>A[j]:
-            min_idx = j
-    A[i],A[min_idx]=A[min_idx],A[i]
-    
-print("Sorted Array")
+	
+	# Find the minimum element in remaining unsorted array
+	min_idx = i
+	for j in range(i+1, len(A)):
+		if A[min_idx] > A[j]:
+			min_idx = j
+			
+	# Swap the found minimum element with the first element		
+	A[i], A[min_idx] = A[min_idx], A[i]
+
+# Driver code to test above
+print ("Sorted array")
 for i in range(len(A)):
-    print("%d"%A[i])
+	print("%d" %A[i]),
