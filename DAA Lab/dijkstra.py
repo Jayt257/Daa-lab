359ead7df40c64b5acca0cb5acfdd2dc21bff83f
shreymakadiya18 <86375553+shreymakadiya18@users.noreply.github.com>
2022-05-04 00:55:24 +0530

    Update dijkstra.py

--- DAA Lab/dijkstra.py
+++ DAA Lab/dijkstra.py
@@ -1,3 +1,4 @@
+# Python program for Dijkstra's single
 class Graph():
 
 	def __init__(self, vertices):
@@ -10,9 +11,13 @@ class Graph():
 		for node in range(self.V):
 			print(node, "\t\t", dist[node])
 
+	# A utility function to find the vertex with minimum distance value, from the set of vertices not yet included in shortest path tree
 	def minDistance(self, dist, sptSet):
+
+		# Initialize minimum distance for next node
 		min = 1e7
 
+		# Search not nearest vertex not in the shortest path tree
 		for v in range(self.V):
 			if dist[v] < min and sptSet[v] == False:
 				min = dist[v]
@@ -20,16 +25,27 @@ class Graph():
 
 		return min_index
 
+	# Function that implements Dijkstra's single source shortest path algorithm for a graph represented using adjacency matrix representation
 	def dijkstra(self, src):
+
 		dist = [1e7] * self.V
 		dist[src] = 0
 		sptSet = [False] * self.V
 
 		for cout in range(self.V):
+
+			# Pick the minimum distance vertex from the set of vertices not yet processed.
+			# u is always equal to src in first iteration
 			u = self.minDistance(dist, sptSet)
 
+			# Put the minimum distance vertex in the
+			# shortest path tree
 			sptSet[u] = True
 
+			# Update dist value of the adjacent vertices
+			# of the picked vertex only if the current
+			# distance is greater than new distance and
+			# the vertex in not in the shortest path tree
 			for v in range(self.V):
 				if (self.graph[u][v] > 0 and
 				sptSet[v] == False and
@@ -38,15 +54,17 @@ class Graph():
 
 		self.printSolution(dist)
 
-
+# Driver program
 g = Graph(9)
+#            0  1  2  3  4  5  6  7  8
 g.graph = [ [0, 4, 0, 0, 0, 0, 0, 8, 0],
-		    [4, 0, 8, 0, 0, 0, 0,11, 0],
-			[0, 8, 0, 7, 0, 4, 0, 0, 2],
-			[0, 0, 7, 0, 9,14, 0, 0, 0],
-			[0, 0, 0, 9, 0,10, 0, 0, 0],
-			[0, 0, 4,14,10, 0, 2, 0, 0],
-			[0, 0, 0, 0, 0, 2, 0, 1, 6],
-			[8,11, 0, 0, 0, 0, 1, 0, 7],
-			[0, 0, 2, 0, 0, 0, 6, 7, 0]]
+	    [4, 0, 8, 0, 0, 0, 0,11, 0],
+	    [0, 8, 0, 7, 0, 4, 0, 0, 2],
+   	    [0, 0, 7, 0, 9,14, 0, 0, 0],
+	    [0, 0, 0, 9, 0,10, 0, 0, 0],
+	    [0, 0, 4,14,10, 0, 2, 0, 0],
+	    [0, 0, 0, 0, 0, 2, 0, 1, 6],
+	    [8,11, 0, 0, 0, 0, 1, 0, 7],
+ 	    [0, 0, 2, 0, 0, 0, 6, 7, 0]]
+
 g.dijkstra(0)
