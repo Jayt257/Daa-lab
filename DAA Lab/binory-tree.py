53ab741f6b35d0c96ffffb7fdf285c077e8212c2
shreymakadiya18 <86375553+shreymakadiya18@users.noreply.github.com>
2022-05-03 22:57:24 +0530

    Update binory-tree.py

--- DAA Lab/binory-tree.py
+++ DAA Lab/binory-tree.py
@@ -9,8 +9,8 @@ def binary_search(arr,low,high,x):
             return mid
         elif arr[mid]>x:
             return binary_search(arr,low,mid-1,x)
-        # else :
-        #     return binary_search(arr,mid+1,high,x)
+        else :
+            return binary_search(arr,mid+1,high,x)
     else:
         return -1
     
